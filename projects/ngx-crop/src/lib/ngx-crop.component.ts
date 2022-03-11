2460825dfcd24178dc6d22d3072d98369fd5f7d7
BenSlimenMohamedAli <dslimen.15@gmail.com>
2022-03-11 22:05:58 +0100

    load image

--- projects/ngx-crop/src/lib/ngx-crop.component.ts
+++ projects/ngx-crop/src/lib/ngx-crop.component.ts
@@ -1,3 +1,4 @@
+import { utils } from './utils';
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
@@ -14,6 +15,11 @@ export class NgxCropComponent implements OnInit {
   // inputs
   @Input() width: number = 200;
   @Input() height: number = 200;
+  @Input() placeholder: string = 'input image';
+
+  // variables
+  selectedImage: string;
+  imageObject: HTMLImageElement;
 
   constructor() {}
 
@@ -23,9 +29,93 @@ export class NgxCropComponent implements OnInit {
     this.context = (this.canvas.nativeElement as HTMLCanvasElement).getContext(
       '2d'
     ) as any;
+
+    this.setTextPlaceholder();
+    this.initialize();
+  }
+
+  setImage() {
+    let image = new Image();
+
+    this.context.lineWidth = 3;
+    this.context.lineCap = 'round';
+    this.context.strokeStyle = '#000';
+    image.onload = () => {
+      this.context.drawImage(
+        image,
+        0,
+        0,
+        this.width,
+        (this.width * image.height) / image.width
+      );
+    };
+    image.src = this.selectedImage;
+
+    this.imageObject = image;
+  }
+
+  initialize() {
+    this.context.imageSmoothingEnabled = true;
+    this.context.imageSmoothingQuality = 'high';
+    this.context.imageSmoothingEnabled = true;
   }
 
   canvasClick() {
-    this.fileInput.nativeElement.click();
+    if (!this.selectedImage) this.fileInput.nativeElement.click();
+  }
+
+  async onSelectImage(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const file = target.files[0];
+
+    this.selectedImage = (await this.toBase64(file)) as string;
+
+    this.setImage();
+  }
+
+  toBase64(file: File): Promise<string | ArrayBuffer> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+    });
+  }
+
+  setTextPlaceholder() {
+    this.context.textBaseline = 'middle';
+    this.context.textAlign = 'center';
+    let fontSize = 10;
+    this.context.font = fontSize + 'px sans-serif';
+    this.context.fillStyle = '#606060';
+    this.context.fillText(this.placeholder, this.width / 2, this.height / 2);
+  }
+
+  onPointerMove(event: Event) {
+    event.preventDefault();
+    let pointerCoord = utils.getPointerCoords(event, {
+      canvas: this.canvas.nativeElement,
+      quality: 1,
+    });
+    let image = new Image();
+
+    this.context.lineWidth = 3;
+    this.context.lineCap = 'round';
+    this.context.strokeStyle = '#000';
+    image.onload = () => {
+      this.context.rect(
+        pointerCoord.x,
+        pointerCoord.y,
+        this.width,
+        (this.width * image.height) / image.width
+      );
+    };
+    image.src = this.selectedImage;
+
+    this.imageObject = image;
+  }
+
+  onDoubleClick(event: Event) {
+    console.log('here');
   }
 }
--- projects/ngx-crop/src/lib/ngx-crop.module.ts
+++ projects/ngx-crop/src/lib/ngx-crop.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxCropComponent } from './ngx-crop.component';
 
 @NgModule({
   declarations: [NgxCropComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   exports: [NgxCropComponent],
 })
 export class NgxCropModule {}
